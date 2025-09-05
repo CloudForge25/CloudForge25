b2995ecf73b96ea739d5018526a3881f6973d8ca
Mr. Ryt <flaviosobbin04@gmail.com>
2025-09-05 13:57:50 +0000

    :sparkles: integrates proxmox api

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./handlers/errorHandler");
 require("dotenv").config();
+const proxmox = require("proxmox")(
+  process.env.PVE_USER,
+  process.env.PVE_PASSWORD,
+  process.env.PVE_URL
+);
 
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:process.env.PORT_NUMBER"],
+    origin: [`http://localhost:${process.env.PORT_NUMBER}`],
   })
 );
 
@@ -15,11 +20,32 @@ app.use(
 
 // Model initializtion
 
+// VMs
+
+proxmox.getClusterStatus(async (err, response) => {
+  if (err) throw err;
+  else {
+    data = JSON.parse(response);
+    console.log(data);
+  }
+});
+
+proxmox.getNodes(async (err, response) => {
+  if (err) throw err;
+  else {
+    data = JSON.parse(response);
+    console.log(data);
+  }
+});
+
+app.use(express.json());
+
 // Routes
 
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to CloudForge!",
+    info: info,
   });
 });
 app.use("*", (req, res, next) => {
